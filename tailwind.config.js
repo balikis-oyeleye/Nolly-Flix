a7ee871d54ea8a3616ff0d8e63cdf5e28fdb2da4
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-05 05:08:56 -1200

    Fix media queries breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "320px",
+      sm: "576px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
     extend: {},
   },
   plugins: [],
