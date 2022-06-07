c69477c4f41110d9928685d62b9b95d6829c1356
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-07 02:16:54 -1200

    Add api

---
+++ src/context/movieContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState } from "react";
+import axios from "axios";
+
+const MovieContext = createContext();
+const baseUrl = "https://api.themoviedb.org/3/";
+const apiKey= process.env.React_APP_API_KEY
+const movieContext = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+
+  const getData = async (searchValue) => {
+    setLoading(true);
+
+    const url = `${baseUrl}${searchValue}?api_key=${process.env.React_APP_API_KEY}&language=en-US`;
+
+    setLoading(false);
+  };
+
+  return <MovieContext.Provider>{children}</MovieContext.Provider>;
+};
+
+export default movieContext;
