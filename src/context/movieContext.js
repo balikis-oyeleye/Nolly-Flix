204605d9f6e443ff92e829dc62f19164223f7af6
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-12 03:34:09 -1200

    Fix errors in movieContext

--- src/context/movieContext.js
+++ src/context/movieContext.js
@@ -1,21 +1,13 @@
 import React, { createContext, useContext, useState } from "react";
 import axios from "axios";
 
-const MovieContext = createContext();
+const Movies = createContext();
+
 const baseUrl = "https://api.themoviedb.org/3/";
 const apiKey = process.env.React_APP_API_KEY;
-const movieContext = ({ children }) => {
-  const [loading, setLoading] = useState(false);
 
-  const getData = async (searchValue) => {
-    setLoading(true);
-
-    const url = `${baseUrl}${searchValue}?api_key=${process.env.React_APP_API_KEY}&language=en-US`;
-
-    setLoading(false);
-  };
-
-  return <MovieContext.Provider>{children}</MovieContext.Provider>;
+const MovieContext = ({ children }) => {
+  return <Movies.Provider value={{}}>{children}</Movies.Provider>;
 };
 
-export default movieContext;
+export default MovieContext;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import movieContext from "./context/movieContext";
+import MovieContext from "./context/movieContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <movieContext>
+    <MovieContext>
       <App />
-    </movieContext>
+    </MovieContext>
   </React.StrictMode>
 );
