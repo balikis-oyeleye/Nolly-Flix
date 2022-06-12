8b426724f8cec51fbac2aab3132192f4a958a651
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-12 08:04:03 -1200

    Update movues.jsx

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Movies = () => {
-  return <div>Movies</div>;
+  const location = useLocation();
+
+  switch (location.pathname) {
+    case "/":
+      return <div className="container mx-auto dark:text-white">Home</div>;
+    case "/trending":
+      return <div className="container mx-auto dark:text-white">Trending</div>;
+    case "/movies":
+      return <div className="container mx-auto dark:text-white">Movies</div>;
+    case "/tv-series":
+      return <div className="container mx-auto dark:text-white">Tv-Series</div>;
+    default:
+      break;
+  }
 };
 
 export default Movies;
