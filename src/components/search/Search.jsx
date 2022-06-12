fbd83142461c35cb216ad50da010173a6985157f
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-12 08:45:41 -1200

    Update search components

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BsX } from "react-icons/bs";
+import "./search.css";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className={`dark:`}>
+      <div className="search-main container mx-auto flex items-center justify-between">
+        <input
+          type="search"
+          placeholder="Search..."
+          className="dark:bg-dark-blue"
+        />
+        <button>
+          <BsX />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
