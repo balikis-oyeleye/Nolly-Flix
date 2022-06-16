3d27aad314615696fb48775e886253926901fd09
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-16 02:42:59 -1200

    Update movie.jsx

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useMovieContext } from "../../context/movieContext";
 
 const Movie = () => {
-  const { movieId, movieName } = useParams();
+  const { movieId } = useParams();
 
-  return (
-    <div>
-      {movieId} {movieName}
-    </div>
-  );
+  const { getMovie, movie } = useMovieContext();
+
+  useEffect(() => {
+    getMovie(movieId);
+  }, []);
+
+  return <div>{movie.title}</div>;
 };
 
 export default Movie;
