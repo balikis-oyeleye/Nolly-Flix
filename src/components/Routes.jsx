3c263aef153f7e129e39ecd511cc53f4b5fb8e21
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-20 00:34:40 -1200

    Update home page path name

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Routes, Route, useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Movies, Movie } from "../components";
 
 const PageRoutes = () => {
   return (
     <div className="dark:bg-dark-blue">
       <Routes>
-        <Route path="/" element={<Movies />} />
+        <Route path="/Nolly-Flix" element={<Movies />} />
         <Route path="/trending" element={<Movies />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/tv-series" element={<Movies />} />
